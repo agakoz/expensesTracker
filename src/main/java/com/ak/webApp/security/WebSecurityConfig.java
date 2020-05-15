30cd31058cf3e674b47377147a6d9e92bfa7de3b
agakoz <agataakoziol@gmail.com>
2020-05-15 18:41:47 +0200

    ssl configuration changed jks -> p12

--- src/main/java/com/ak/webApp/security/WebSecurityConfig.java
+++ src/main/java/com/ak/webApp/security/WebSecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -34,8 +36,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /*
         http.csrf().disable();
         http.headers().disable();
+
+         */
+        http.requiresChannel()
+                .anyRequest()
+                .requiresSecure();
         http.authorizeRequests()
                 .antMatchers("/users/**").hasAuthority("ROLE_USER")
                 .antMatchers("/expenses/**").hasAuthority("ROLE_USER")
